2fb798df2dff7bb0ce91e76a05efdd8fc682b640
Viachaslau Biziukin <biv@unicsoft.com>
2023-02-09 16:19:14 +0300

    Add FSD base Architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import './styles/index.scss';
 import { Route, Routes } from 'react-router-dom';
-import { AboutPageAsync } from './Pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './Pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 
 const App = () => {
@@ -19,8 +19,8 @@ const App = () => {
 
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={'/about'} element={<AboutPageAsync />}/>
-          <Route path={'/'} element={<MainPageAsync />}/>
+          <Route path={'/about'} element={<AboutPage />}/>
+          <Route path={'/'} element={<MainPage />}/>
         </Routes>
       </Suspense>
     </div>
