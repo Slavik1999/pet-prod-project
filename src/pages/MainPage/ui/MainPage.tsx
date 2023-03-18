b4e75eaa1901e97e38ec7e03ad919c1ddebc0b21
Viachaslau Biziukin <biv@unicsoft.com>
2023-03-18 21:42:14 +0300

    Stylelint, i18n plugin for eslint

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,15 +1,14 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
-  const {t} = useTranslation('main');
+    const { t } = useTranslation('main');
 
-
-  return (
-    <div>
-      {t("Главная страница")}
-      {t("Главная страница 123")}
-    </div>
-  );
+    return (
+        <div>
+            {t('Главная страница')}
+            {t('Главная страница 123')}
+        </div>
+    );
 };
 
 export default MainPage;
