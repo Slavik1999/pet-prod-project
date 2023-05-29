1f282918c655dff0c5b95c1e2a42a8a5fd8c52b4
Viachaslau Biziukin <biv@unicsoft.com>
2023-05-29 19:36:46 +0300

    LESSON-23-24; Bundle analyzer; Jest for testing components

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,9 +33,20 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         'max-len': ['error', { code: 140, ignoreComments: true }],
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': [
+            'error',
+            { markupOnly: true, ignoreAttribute: ['data-testid'] },
+        ],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts, tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            },
+        },
+    ],
 };
